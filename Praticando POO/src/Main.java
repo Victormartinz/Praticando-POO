0cc0b899ab094c9881c4e8524e08ea2b0753a95b
VictorMs <martinssantosvictor@gmail.com>
2026-03-14 21:33:47 -0300

    criando o esqueleto da classe Main para receber a class Carro

--- Praticando POO/src/Main.java
+++ Praticando POO/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Criando o primeiro Carro objeto: Civic
+        Carro Civic = new Carro();
+        Civic.marca = "Honda";
+        Civic.modelo = "Civic";
+        Civic.ano = 2012;
+
+        //Criando o segundo Carro objeto: Corolla
+        Carro Corolla = new Carro();
+        Corolla.marca = "Toyota";
+        Corolla.modelo =  "Corolla";
+        Corolla.ano = 2015;
+
+
 
     }
 }
