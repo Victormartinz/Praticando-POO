5ddc3b473fd60f00fdba1f4cd2856e5d224be686
VictorMs <martinssantosvictor@gmail.com>
2026-03-15 15:54:24 -0300

    Criando um objeto chamado noites brancas e usando o metodo para chamar e exibir os atributos(caracterisiticas)

--- Praticando POO - Livros/src/Livros.java
+++ Praticando POO - Livros/src/Livros.java
@@ -1,3 +1,12 @@
 public class Livros {
 
+    String titulo;
+    int numeroPaginas;
+    String autor;
+
+    public void exibirInformacoes (){
+        System.out.println("Livro: " + titulo +  " Autor: " + autor + " Número De Páginas: " + numeroPaginas);
+
+    }
+
 }
--- Praticando POO - Livros/src/Main.java
+++ Praticando POO - Livros/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        //criando um objeto chamado noites brancas, um livro classico
+        Livros noitesBrancas = new Livros();
+        noitesBrancas.titulo = "Noites Brancas";
+        noitesBrancas.autor = "Fiódor Dostoiévsk";
+        noitesBrancas.numeroPaginas = 96;
+
+        //chamando o metodo de exibição para o objeto noites brancas
+
+
     }
 }
+
