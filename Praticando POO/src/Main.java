4a6fba94bdef7dfbb34661ab9f8e1811c0d34512
VictorMs <martinssantosvictor@gmail.com>
2026-03-14 22:03:10 -0300

    aqui eu terminei, adicionando os metodos para exibição dos objetos(carros) e o tempo de uso

--- Praticando POO/src/Main.java
+++ Praticando POO/src/Main.java
@@ -7,13 +7,29 @@ public class Main {
         Civic.modelo = "Civic";
         Civic.ano = 2012;
 
+        System.out.println("=================================");
+
+        // Chamando o metodo de exibição para o civic
+        Civic.exibirDetalhe();
+
+        //chamando o metodo para calcular o tempo de uso
+        int tempoDeUsoCivic = Civic.calcularIdade(2020);
+        System.out.println("Seu Civic tem " + tempoDeUsoCivic + " Anos de Uso");
+
         //Criando o segundo Carro objeto: Corolla
         Carro Corolla = new Carro();
         Corolla.marca = "Toyota";
         Corolla.modelo =  "Corolla";
         Corolla.ano = 2015;
 
+        System.out.println("=================================");
 
+        // Chamando o metodo de exibição para o corolla
+        Corolla.exibirDetalhe();
+
+        //chamando o metodo para calcular o tempo de uso
+        int tempoDeUsoCorolla = Corolla.calcularIdade(2020);
+        System.out.println("Seu Corolla tem " + tempoDeUsoCorolla + " Anos de Uso");
 
     }
 }
